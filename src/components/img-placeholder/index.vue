d80366c743a766cf981daf8d6b355c86f82ffd9f
konojunya <junya03dance@gmail.com>
2018-02-05 18:36:06 +0900

    hgoe

---
+++ src/components/img-placeholder/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div ref="wrapper"></div>
+</template>
+
+<script>
+export default {
+  props: ["src"],
+  mounted() {
+    const wrapper = this.$refs.wrapper
+    wrapper.style.width = "500px";
+    wrapper.style.height = "200px";
+    wrapper.style.backgroundColor = "gray"
+
+    const image = new Image();
+    image.onload = () => {
+      image.width = 500
+      this.$refs.wrapper.appendChild(image)
+      this.$forceUpdate()
+    }
+
+    setTimeout(() => {
+      image.src = this.src
+    }, 1000)
+  }
+}
+</script>
---
+++ src/components/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <img-placeholder src="http://www.jtb.co.jp/kaigai/area/hawaii/images/img_main_visual_01_renew.jpg" ></img-placeholder>
+</template>
+
+<script>
+import ImgPlaceholder from './img-placeholder/index.vue'
+
+export default {
+  components: {
+    ImgPlaceholder
+  }
+}
+</script>
